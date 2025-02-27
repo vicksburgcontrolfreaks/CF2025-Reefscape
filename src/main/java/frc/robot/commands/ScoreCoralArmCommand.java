8c48590793cbfc0d8544ec76e0cc9c527039e56a
distorbed <distorbed674@gmail.com>
2025-02-26 20:42:44 -0500

    score coral command

--- src/main/java/frc/robot/commands/ScoreCoralArmCommand.java
+++ src/main/java/frc/robot/commands/ScoreCoralArmCommand.java
@@ -7,26 +7,26 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.NewCoralArmSubsystem;
 import frc.robot.Constants.ArmConstants;
 
-import javax.swing.text.Position;
-
-import edu.wpi.first.wpilibj.XboxController;
 
 public class ScoreCoralArmCommand extends Command {
     private final NewCoralArmSubsystem m_armSubsystem;
-    private int m_position;
-    private int m_position_prev = -1;
-    private int counter;
 
     /**
      * Constructs a command for manual coral arm adjustment.
      * @param armSubsystem The coral arm subsystem.
      */
 
-    public ScoreCoralArmCommand(NewCoralArmSubsystem armSubsystem, int position) {
+    public ScoreCoralArmCommand(NewCoralArmSubsystem armSubsystem) {
         m_armSubsystem = armSubsystem;
-        m_position = position;
         addRequirements(m_armSubsystem);
     }
+
+    // Low target scoring sequence 
+    // step 1: set angle to 44
+    // step 2: set extension to -24
+    // step 3: set angle to 30
+
+    
 
     @Override
     public void execute() {
@@ -37,37 +37,6 @@ public class ScoreCoralArmCommand extends Command {
 
     @Override
     public boolean isFinished() {
-
-        if (m_position != m_position_prev)
-        {
-            counter = 0;
-        }
-
-        counter++;
-        
-        switch (m_position) {
-            case ArmConstants.TGT_LOW:
-                if (counter >= 100)
-                {
-                    return true;
-                }
-                break;
-            case ArmConstants.TGT_MID:
-                if (counter >= 150)
-                {
-                    return true;
-                }   
-                break;
-            case ArmConstants.TGT_HIGH:
-                if (counter >= 200)
-                {
-                    return true;
-                }
-                break;
-        }
-
-        m_position_prev = m_position;
-
         return false;
     }
 
