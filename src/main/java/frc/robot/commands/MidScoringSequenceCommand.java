0ebe27c3c078e01b14a448e7b6fd35d0864f4904
distorbed <distorbed674@gmail.com>
2025-03-14 10:26:38 -0400

    comp changes 3

--- src/main/java/frc/robot/commands/MidScoringSequenceCommand.java
+++ src/main/java/frc/robot/commands/MidScoringSequenceCommand.java
@@ -22,8 +22,6 @@ public class MidScoringSequenceCommand extends SequentialCommandGroup {
      * @param armSubsystem The NewCoralArmSubsystem controlling the arm.
      */
 
-    private final double waitTime = 0.2;
-
     public MidScoringSequenceCommand(NewCoralArmSubsystem armSubsystem) {
         addCommands(
             new InstantCommand(() -> armSubsystem.setArmAngle(.28), armSubsystem),
@@ -34,7 +32,7 @@ public class MidScoringSequenceCommand extends SequentialCommandGroup {
             new WaitCommand(0.2),
             new InstantCommand(() -> armSubsystem.moveArm(-35), armSubsystem),
             new WaitCommand(0.2),
-            new InstantCommand(() -> armSubsystem.setArmAngle(.25), armSubsystem),
+            new InstantCommand(() -> armSubsystem.setArmAngle(.27), armSubsystem),
             new WaitCommand(0.2),
             //new InstantCommand(() -> armSubsystem.moveArm(-50), armSubsystem),
             //new WaitCommand(0.8),
